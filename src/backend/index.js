ec671e803f33b3c06e6c8770e3c33555cdfeaf6a
Joe Smith <joesmith0488@gmail.com>
2016-01-17 12:38:23 +0000

    reduced webpack configs to 1 file

--- src/backend/index.js
+++ src/backend/index.js
@@ -11,7 +11,7 @@ app.listen(port, () =>
 if (process.env.NODE_ENV === 'development') {
   const webpack = require('webpack');
   const WebpackDevServer = require('webpack-dev-server');
-  const config = require('../../webpack.dev.config');
+  const config = require('../../webpack.config');
 
   new WebpackDevServer(webpack(config), {
     publicPath: config.output.publicPath,
